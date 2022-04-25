4f751c18f532212265e5409b796ff46e5e1a93a4
InsTiport <hfsun98@gmail.com>
2022-04-25 00:06:27 -0400

    modify ways to split data

--- itemRank_main.py
+++ itemRank_main.py
@@ -5,23 +5,47 @@ Credit: adapted from https://github.com/arashkhoeini/itemrank
 import tqdm
 import numpy as np
 from ItemRank import ItemRank
-from numpy.random import default_rng
 
-rng = default_rng()
+
+def uniquify(a):
+    unique_tuples = set()
+    for row in a:
+        unique_tuples.add(tuple(row))
+
+    unique_tuples = list(unique_tuples)
+    unique_tuples = [list(t) for t in unique_tuples]
+
+    return np.array(unique_tuples)
+
 
 # read train and test data
-user_subreddit_rating_list = []
+user_subreddit_rating = dict()
 with open('user_subreddit.tsv', 'r') as r:
     line_counter = 0
     for line in tqdm.tqdm(r.readlines()):
         if line_counter >= 1:
             splits = line.split('\t')
             user, subreddit, rating = int(splits[0]), int(splits[1]), float(splits[2])
-            user_subreddit_rating_list.append([user, subreddit, rating])
+            user_subreddit_rating[user, subreddit] = rating
 
         line_counter += 1
 
-all_data = np.array(user_subreddit_rating_list)
+all_data_list = []
+line_counter = 0
+with open('reddit.csv') as r:
+    for line in tqdm.tqdm(r.readlines()):
+        line_counter += 1
+
+        # exclude header
+        if line_counter > 1:
+            # get all data attributes
+            split = line.split(',')
+            user_id, subreddit_id = int(split[0]), int(split[1])
+
+            all_data_list.append([user_id, subreddit_id, user_subreddit_rating[user_id, subreddit_id]])
+
+all_data = np.array(all_data_list)
+unique_all_data = uniquify(all_data)
 
 print('Training and evaluating...\n')
 for split in range(5):
@@ -33,8 +57,11 @@ for split in range(5):
     np_data = all_data[training_idx]
     np_test_data = all_data[test_idx]
 
+    np_data = uniquify(np_data)
+    np_test_data = uniquify(np_test_data)
+
     # training and testing the model
-    item_rank = ItemRank(np_data, all_data)
+    item_rank = ItemRank(np_data, unique_all_data)
     item_rank.generate_graph()
     item_rank.generate_coef_from_graph()
     DOAs = []
--- split_train_test.py
+++ split_train_test.py
@@ -1,28 +1,37 @@
 import tqdm
 import numpy as np
-from numpy.random import default_rng
 
-rng = default_rng()
+# # read train and test data
+# user_subreddit_rating_list = []
+# with open('user_subreddit.tsv', 'r') as r:
+#     line_counter = 0
+#     for line in tqdm.tqdm(r.readlines()):
+#         if line_counter >= 1:
+#             splits = line.split('\t')
+#             user, subreddit, rating = int(splits[0]), int(splits[1]), float(splits[2])
+#             user_subreddit_rating_list.append([user, subreddit, rating])
+#
+#         line_counter += 1
 
-# read train and test data
-user_subreddit_rating_list = []
-with open('user_subreddit.tsv', 'r') as r:
-    line_counter = 0
+line_count = 0
+with open('reddit.csv') as r:
     for line in tqdm.tqdm(r.readlines()):
-        if line_counter >= 1:
-            splits = line.split('\t')
-            user, subreddit, rating = int(splits[0]), int(splits[1]), float(splits[2])
-            user_subreddit_rating_list.append([user, subreddit, rating])
+        line_count += 1
 
-        line_counter += 1
 
-all_data = np.array(user_subreddit_rating_list)
-print('Splitting data...')
-for i in range(5):
-    training_idx = rng.choice(len(all_data), int(0.8 * len(all_data)), replace=False)
-    test_idx = np.array(([i for i in range(len(all_data)) if i not in training_idx]))
-    rng.shuffle(test_idx)
-    with open(f'split_{i}.txt', 'w') as w:
+def get_split(size, train_proportion=0.8, seed=0):
+
+    train_size = round(size * train_proportion)
+    np.random.seed(seed)
+    train_ids = np.random.choice(np.arange(size), train_size, replace=False)
+    test_ids = np.setdiff1d(np.arange(size), train_ids)
+
+    return train_ids, test_ids
+
+
+for split in range(5):
+    training_idx, test_idx = get_split(line_count - 1, seed=split)
+    with open(f'split_{split}.txt', 'w') as w:
         for j, idx in enumerate(training_idx):
             w.write(str(idx))
             if j != len(training_idx) - 1:
